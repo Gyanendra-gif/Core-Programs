39d50449582719f9b42ec06df0c7e66d8efc605d
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-29 19:26:52 +0530

    [Gyanendra] Add: A Program of Prime Factors

---
+++ core_programs/Factors.py
@@ -0,0 +1,31 @@
+'''
+@Author: Gyanendra
+@Date: 2021-12-29
+@Last Modified by: Gyanendra
+@Last Modified time: 2021-12-29 
+@Title : Prime Factorization of a Number
+'''
+
+"""
+Description:
+    Function description in depth
+Parameter:
+      Taking Input From User and Calculate its Prime Factor
+Return:
+      Prime Factor of a Number
+"""
+number = int(input('Enter the Number:' ))
+temp = number
+factors =[]
+while temp != 1:
+    for i in range (2, number):
+        if temp % i == 0:
+            while temp % i == 0:
+                factors.append(i)
+                temp = temp/i
+            temp=1
+
+if len(factors) == 0:
+    print(str(number) + ' is a Prime Number')
+else:
+    print(factors)  
