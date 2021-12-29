7e1910168001755fc25a42efd2336f7d432a7e08
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-29 12:48:01 +0530

    [Gyanendra] Add: A Program of Coin Flip

---
+++ core_programs/CoinFlip.py
@@ -0,0 +1,33 @@
+'''
+@Author: Gyanendra
+@Date: 2021-12-29
+@Last Modified by: Gyanendra
+@Last Modified time: 2021-12-29 
+@Title : Take User Input and Replace it with String
+'''
+
+"""
+Description:
+    Function description in depth
+Parameter:
+      Taking Input From User as No. of Times Coin Should be Flip
+Return:
+      Percentage of Head and Tails i Flip
+"""
+import random
+headCount = 0
+tailCount = 1
+count = int(input('How Many Time You Wants to Flip Coin'))
+for x in range (count) :
+    flip = random.random()
+if flip < 0.5 :
+    print('Head Occures')
+    headCount = headCount + 1
+else:
+    print('Tails Occures')
+    tailCount = tailCount + 1
+
+headPercentage = int(headCount / count * 100)
+tailPercentage = 100 - headPercentage
+print('Head Percentage is: ' + str(headPercentage))
+print('Tail Percentage is: ' + str(tailPercentage))
