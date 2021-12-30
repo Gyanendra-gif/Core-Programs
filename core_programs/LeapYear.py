05b6b6ae96d0a6018dd7fd564aa9a6d2517fcaf7
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-30 13:28:35 +0530

    [Gyanendra] Refactor: A Program of Leap Year

--- core_programs/LeapYear.py
+++ core_programs/LeapYear.py
@@ -3,7 +3,7 @@
 @Date: 2021-12-29
 @Last Modified by: Gyanendra
 @Last Modified time: 2021-12-29 
-@Title : Take User Input and Replace it with String
+@Title : Take User Input Year and Check for Its Leap Year or Not
 '''
 
 """
@@ -14,8 +14,11 @@ Parameter:
 Return:
       Leap Year or Non Leap Year
 """
-year = int(input('Enter the Year to Check and it should be in four digit'))
-if year % 4 == 0 and year % 100 != 0 or year % 400 == 0 :
-    print('Year in a Leap Year')
-else: 
-    print('Year is Not a Leap Year')    
+year = input('Enter the Year to Check and it should be in four digit: ')
+while (len(year) == 4):
+    if int(year) % 4 == 0 and int(year) % 100 != 0 or int(year) % 400 == 0 :
+        print('Year in a Leap Year', int(year))
+        break
+    else: 
+        print('Year is Not a Leap Year', int(year))
+        break    
