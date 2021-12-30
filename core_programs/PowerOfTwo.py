04db4de4d40556bd7701c2a72d9af694c1d2b3d2
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-30 13:12:33 +0530

    [Gyanendra] Refactor: A Program of Power Of Two

--- core_programs/PowerOfTwo.py
+++ core_programs/PowerOfTwo.py
@@ -3,7 +3,7 @@
 @Date: 2021-12-29
 @Last Modified by: Gyanendra
 @Last Modified time: 2021-12-29 
-@Title : Take User Input and Replace it with String
+@Title : Take User Input and Provide Power of two
 '''
 
 """
@@ -14,9 +14,10 @@ Parameter:
 Return:
       Power of Two 
 """
-exponent = int(input('Enter N to Check Power of Two'))
-list = []
-for i in range (1, exponent+1) :
-    list.append(int(i**2))
+exponent = int(input('Enter N to Check Power of Two '))
+if (0<=exponent<31):
+    for i in range (1, exponent+1):
+        print('2 ^',i,'=', pow(2, i))
 
-print(list)
+else:
+    print('Please Provide Exponent in Range Of 0 to 31')
