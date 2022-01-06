7f35c10ba51a00056c4459c2d808a4deef95f406
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-06 07:04:28 +0530

    [Gyanendra] Refactor: A Program of Array is Refactored

--- data_structure/Array/ArrayOperation.py
+++ data_structure/Array/ArrayOperation.py
@@ -2,7 +2,7 @@
 @Author: Gyanendra
 @Date: 04/01/2021 
 @Last Modified by: Gyanendra
-@Last Modified time: 05/01/2021 
+@Last Modified time: 06/01/2021 
 @Title : Array in Python
 """
 from array import *
@@ -27,9 +27,7 @@ def occurence_array(number, element):
         if i == element:
             count = count + 1
             break
-    result = print(count)
-    return result
-
+    print(count)
 
 # Remove First Occurence
 def removeOccurence_array(number, element):
@@ -38,31 +36,41 @@ def removeOccurence_array(number, element):
         if i == element:
             result = number.remove(element)
             break
-    result = print(number)
-    return result
+    print(number)   
 
 if __name__ == "__main__":
+    """
+Description:
+    Function is Used for Operate the Array Operation after taking input from user
+    and it will return the respective output
+"""
+    
     condition = True
     while (condition == True):
-
-        choice = int(input("Enter the Option to Execute Array Operation Press"
+        try:
+            choice = int(input("Enter the Option to Execute Array Operation Press"
                                                                             + "\n 1: To Print Array"
                                                                             + "\n 2: To Reverse Array" 
                                                                             + "\n 3: To Count Occurence of Element"
                                                                             + "\n 4: To Remove Occurence: "))
-        if choice == 1:
-            print_array(number)
-        elif choice == 2:
-            reverse_array(number)
-        elif choice == 3:
-            element = int(input("Enter The Element Whose Occurence is to be Check: "))
-            occurence_array(number, element)
-        elif choice == 4:
-            element = int(input("Enter The Element Whose Occurence is to be Check: "))
-            removeOccurence_array(number, element)
-        else:
-            print('Provide Correct Input in Range')
-        userChoice = int(input('Press 0 to Continue the Program or 1 to stop the program: '))
-        if (userChoice == 1):
-            condition = False         
+        
+            
+            if choice == 1:
+                print_array(number)
+            elif choice == 2:
+                reverse_array(number)
+            elif choice == 3:
+                element = int(input("Enter The Element Whose Occurence is to be Check: "))
+                occurence_array(number, element)
+            elif choice == 4:
+                element = int(input("Enter The Element Whose Occurence is to be Remove: "))
+                removeOccurence_array(number, element)
+            else:
+                print('Provide Correct Input in Range')
+                
+            userChoice = int(input('Press 0 to Continue the Program or 1 to stop the program: '))
+            if (userChoice == 1):
+                condition = False    
+        except ValueError:
+            print("Error!, Entered Value is not Integer")             
 
