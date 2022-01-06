67af2466ef40e80a35c9c5e98c159653c6daf553
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-06 07:04:28 +0530

    [Gyanendra] Refactor: A Program of Array is Refactored

--- data_structure/Array/ArrayOperation.py
+++ data_structure/Array/ArrayOperation.py
@@ -44,25 +44,29 @@ def removeOccurence_array(number, element):
 if __name__ == "__main__":
     condition = True
     while (condition == True):
-
-        choice = int(input("Enter the Option to Execute Array Operation Press"
+        try:
+            choice = int(input("Enter the Option to Execute Array Operation Press"
                                                                             + "\n 1: To Print Array"
                                                                             + "\n 2: To Reverse Array" 
                                                                             + "\n 3: To Count Occurence of Element"
                                                                             + "\n 4: To Remove Occurence: "))
-        if choice == 1:
-            print_array(number)
-        elif choice == 2:
-            reverse_array(number)
-        elif choice == 3:
-            element = int(input("Enter The Element Whose Occurence is to be Check: "))
-            occurence_array(number, element)
-        elif choice == 4:
-            element = int(input("Enter The Element Whose Occurence is to be Check: "))
-            removeOccurence_array(number, element)
-        else:
-            print('Provide Correct Input in Range')
-        userChoice = int(input('Press 0 to Continue the Program or 1 to stop the program: '))
-        if (userChoice == 1):
-            condition = False         
+        
+            
+            if choice == 1:
+                print_array(number)
+            elif choice == 2:
+                reverse_array(number)
+            elif choice == 3:
+                element = int(input("Enter The Element Whose Occurence is to be Check: "))
+                occurence_array(number, element)
+            elif choice == 4:
+                element = int(input("Enter The Element Whose Occurence is to be Remove: "))
+                removeOccurence_array(number, element)
+            else:
+                print('Provide Correct Input in Range')
+            userChoice = int(input('Press 0 to Continue the Program or 1 to stop the program: '))
+            if (userChoice == 1):
+                condition = False    
+        except ValueError:
+            print("Error!, Entered Value is not Integer")             
 
