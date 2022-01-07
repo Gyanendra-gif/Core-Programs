4084245c26f10cb9c738e227504640fbf245b58c
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-07 16:17:54 +0530

    [Gyanendra] Refactor: Array Program is Refactored

--- data_structure/Array/ArrayOperation.py
+++ data_structure/Array/ArrayOperation.py
@@ -8,7 +8,15 @@
 from array import *
 
 # Array Creation
-number = array("i", [1,2,3,4,5])
+# number = array("i", [])
+def create_array():
+    number = array("i", [])
+    element = int(input("Enter The Number of Elements to Add is Array: "))
+    for i in range (0, element):
+        index = int(input("Enter Number at Index: "))
+        number.append(index)
+        print("Created Array is: ", number) 
+        return number       
 
 # Print Array
 def print_array(number):
@@ -39,6 +47,7 @@ def removeOccurence_array(number, element):
     print(number)   
 
 if __name__ == "__main__":
+    
     """
 Description:
     Function is Used for Operate the Array Operation after taking input from user
@@ -49,22 +58,24 @@ Description:
     while (condition == True):
         try:
             choice = int(input("Enter the Option to Execute Array Operation Press"
-                                                                            + "\n 1: To Print Array"
-                                                                            + "\n 2: To Reverse Array" 
-                                                                            + "\n 3: To Count Occurence of Element"
-                                                                            + "\n 4: To Remove Occurence: "))
+                                                                            + "\n 1: To Create Array"
+                                                                            + "\n 2: To Print Array"
+                                                                            + "\n 3: To Reverse Array" 
+                                                                            + "\n 4: To Count Occurence of Element"
+                                                                            + "\n 5: To Remove Occurence: "))
         
-            
             if choice == 1:
-                print_array(number)
+                value = create_array()
             elif choice == 2:
-                reverse_array(number)
+                print_array(value)
             elif choice == 3:
-                element = int(input("Enter The Element Whose Occurence is to be Check: "))
-                occurence_array(number, element)
+                reverse_array(value)
             elif choice == 4:
+                element = int(input("Enter The Element Whose Occurence is to be Check: "))
+                occurence_array(value, element)
+            elif choice == 5:
                 element = int(input("Enter The Element Whose Occurence is to be Remove: "))
-                removeOccurence_array(number, element)
+                removeOccurence_array(value, element)
             else:
                 print('Provide Correct Input in Range')
             userChoice = int(input('Press 0 to Continue the Program or 1 to stop the program: '))
