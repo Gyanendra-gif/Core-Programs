7862c3e18312c2382562069fffc2a89358eaae76
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-10 17:39:36 +0530

    [Gyanendra] Add: A Program of Concatenate List Elements into string

---
+++ basic_programs/Nine_concatenateList.py
@@ -0,0 +1,27 @@
+"""
+@Author: Gyanendra
+@Date: 10/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 10/01/2021 
+@Title : Basic Programs in Python
+"""
+
+def function_concate_elements(list):
+    """
+Description:
+    Function is Used for taking list as a parameter and print the symbol according to that 
+Parameter:
+    list
+Return:
+    Print the pattern according to list
+"""
+    data = ' '
+    for element in list:
+        data += str(element)
+    return data
+
+if __name__ == "__main__":
+    my_list = ['Welcome','to','Bridgelabz','Family']
+    result = function_concate_elements(my_list)
+    print(result)
+
