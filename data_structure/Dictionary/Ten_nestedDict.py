b39825acb5b8744058f0f1c68fae330b519d334f
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-10 22:18:13 +0530

    [Gyanendra] Add: A Program of Nested Dictionary is Added

---
+++ data_structure/Dictionary/Ten_nestedDict.py
@@ -0,0 +1,26 @@
+"""
+@Author: Gyanendra
+@Date: 10/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 10/01/2021 
+@Title : Dictionary in Python
+"""
+
+def function_nested_dict(list):
+    """
+Description:
+    Function is Used for Adding Data in Dictionary
+Parameter:
+    Dictionary name to add key and values in it
+Return:
+    Updated Dictionary
+"""
+    updated_dict = my_dict = {}
+    for data in list:
+        my_dict[data] = {}
+        my_dict = my_dict[data]
+    print("Here is Nested Dict: " ,updated_dict)
+
+if __name__ == "__main__":
+    sample_list = [1,2,3,4,5,6]
+    function_nested_dict(sample_list)
