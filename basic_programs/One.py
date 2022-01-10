39d017e8f0d9f7b68209be27eafeaf21b567c98a
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-10 08:44:09 +0530

    [Gyanendra] Refactor: A Program of Reversed Name is Added

--- basic_programs/One.py
+++ basic_programs/One.py
@@ -3,7 +3,7 @@
 @Date: 09/01/2021 
 @Last Modified by: Gyanendra
 @Last Modified time: 09/01/2021 
-@Title : Dictionary in Python
+@Title : Basic Programs in Python
 """
 
 def function_reverse_name():
---
+++ basic_programs/Two.py
@@ -0,0 +1,7 @@
+"""
+@Author: Gyanendra
+@Date: 09/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 09/01/2021 
+@Title : Dictionary in Python
+"""
