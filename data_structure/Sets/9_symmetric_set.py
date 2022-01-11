53c578151bc9e0add5ab4a90c31c41cd10c62abf
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-11 22:58:10 +0530

    [Gyanendra] Add: A Program of Symmetric Difference of Two Set is added

---
+++ data_structure/Sets/9_symmetric_set.py
@@ -0,0 +1,26 @@
+"""
+@Author: Gyanendra
+@Date: 11/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 11/01/2021 
+@Title : Set in Python
+"""
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def function_union_set(setOne, setTwo):
+    """
+    Description:
+        Function is Used for Difference of two Sets
+    Parameter:
+        Sample Set
+    Return:
+        Updated Set
+    """
+    result = setOne.symmetric_difference(setTwo)
+    return result
+
+sample_set_one = {'Black','Blue', 'Green', 'Yellow'}
+sample_set_two = {'Red', 'Pink', 'Orange', 'Blue','Black'}
+output = function_union_set(sample_set_one,sample_set_two)
+logging.debug("Here is the Updated Set is {} ".format(output))  
