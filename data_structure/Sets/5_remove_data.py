ed04e90d24e01818233a545bd336c461338c7e5e
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-11 22:30:07 +0530

    [Gyanendra] Refactor: A Program of remove data from Set if that is available in set

---
+++ data_structure/Sets/5_remove_data.py
@@ -0,0 +1,32 @@
+"""
+@Author: Gyanendra
+@Date: 11/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 11/01/2021 
+@Title : Set in Python
+"""
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def function_remove_dataSet(set):
+    """
+    Description:
+        Function is Used for Removing Items from set
+    Parameter:
+        Sample Set
+    Return:
+        Updated Set
+    """
+    user_input = input("Enter the Name to Remove from set ")
+    list = user_input.split()
+    for i in list:
+        try:
+            set.remove(i)
+        except KeyError:
+            print('Error! Data is not available is set')
+            continue
+    return set
+
+sample_set = {'Sam', 'Tom', 'Harry'}
+output = function_remove_dataSet(sample_set)
+logging.debug("Here is the Updated Set is {} ".format(sample_set ,output))  
