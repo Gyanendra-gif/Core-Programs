4b85447b27b8e64d0c71d947862362ab34f2b351
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-11 12:24:59 +0530

    [Gyanendra] Refactor: A Program of Sort by Values is Refactored

--- data_structure/Dictionary/Seven_iterate.py
+++ data_structure/Dictionary/Seven_iterate.py
@@ -6,10 +6,19 @@
 @Title : Basic Programs in Python
 """
 def sort_by_values(dict):
+    """
+    Description:
+        Function is Used for Counting Letters From Sample String
+    Parameter:
+        String to Count its Letters
+    Return:
+        Dictionary with having letters in it
+    """
     for key in dict:
         print(key, 'Corresponds to', dict[key])
 
-if __name__ == "__main___":
-    my_dict = {'Tom': 1, 'Sam': 2, 'Ram': 3}
+if __name__ == "__main__":
+
+    my_dict = {1:'Tom', 2:'Sam'}
     sort_by_values(my_dict)
     
