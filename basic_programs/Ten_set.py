887a492518d3367654b3e70305cebb28d9b45aa3
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-11 14:43:20 +0530

    [Gyanendra] Refactor: A Program to Print Symbol in a given format according to list elements

--- basic_programs/Ten_set.py
+++ basic_programs/Ten_set.py
@@ -8,13 +8,13 @@
 
 def function_set_color():
     """
-Description:
-    Function is Used for taking list as a parameter and print the symbol according to that 
-Parameter:
-    list
-Return:
-    Print the pattern according to list
-"""
+    Description:
+        Function is Used for taking list as a parameter and print the symbol according to that 
+    Parameter:
+        list
+    Return:
+        Print the pattern according to list
+    """
     color_list_1 = set(["White", "Black", "Red"])
     color_list_2 = set(["Red", "Green"])
     color_list_3 = (color_list_1.difference(color_list_2))
