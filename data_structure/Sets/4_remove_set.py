2bc2e7501a91be560176ee35b415243a17a48176
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-11 22:20:45 +0530

    [Gyanendra] Refactor: A Program of remove data from Set is Added

--- data_structure/Sets/4_remove_set.py
+++ data_structure/Sets/4_remove_set.py
@@ -3,7 +3,7 @@
 @Date: 11/01/2021 
 @Last Modified by: Gyanendra
 @Last Modified time: 11/01/2021 
-@Title : Dictionary in Python
+@Title : Set in Python
 """
 import logging
 logging.basicConfig(level=logging.DEBUG)
