ef844510f2a7867d8ec3f457908447666cb5f9c8
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-11 14:45:46 +0530

    [Gyanendra] Refactor: A Program of Call of Method is Refactored

--- basic_programs/Twelve_callMethod.py
+++ basic_programs/Twelve_callMethod.py
@@ -2,21 +2,21 @@
 @Author: Gyanendra
 @Date: 10/01/2021 
 @Last Modified by: Gyanendra
-@Last Modified time: 10/01/2021 
+@Last Modified time: 11/01/2021 
 @Title : Basic Programs in Python
 """
 import os
 
 def function_callExternal_method():
     """
-Description:
-    Function is Used for calling outer method after importing os module 
-Parameter:
-    None
-Return:
-    None
-"""
-    print(os.system('ls -l'))
+    Description:
+        Function is Used for calling outer method after importing os module 
+    Parameter:
+        None
+    Return:
+        None
+    """
+    print(os.system('cd ..'))
 
 if __name__ == "__main__":
     function_callExternal_method()    
