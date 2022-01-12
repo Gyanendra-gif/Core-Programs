868d16ce3e224dd5038722b767b365969b26267f
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-12 08:00:50 +0530

    [Gyanendra] Add: A Program of Finding Max and Min value of set is added

---
+++ data_structure/Sets/12_max_min_set.py
@@ -0,0 +1,40 @@
+"""
+@Author: Gyanendra
+@Date: 11/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 11/01/2021 
+@Title : Set in Python
+"""
+import logging
+from typing import FrozenSet
+logging.basicConfig(level=logging.DEBUG)
+
+def function_max_set(setOne):
+    """
+    Description:
+        Function is Used for finding max value of Set
+    Parameter:
+        Sample Set
+    Return:
+        Updated Set
+    """
+    result = max(setOne)
+    return result
+
+def function_min_set(setOne):
+    """
+    Description:
+        Function is Used for finding min value of Set
+    Parameter:
+        Sample Set
+    Return:
+        Updated Set
+    """
+    result = min(setOne)
+    return result    
+
+sample_set_one = {23,45,65,12,87,96,2,4}
+max_value = function_max_set(sample_set_one)
+min_value = function_min_set(sample_set_one)
+logging.debug("Here is the Max Value of Set is {} ".format(max_value)) 
+logging.debug("Here is the Min Value of Set is {} ".format(min_value)) 
