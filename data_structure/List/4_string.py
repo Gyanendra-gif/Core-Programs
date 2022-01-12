53daf53b9ab6065cb5b41d0c8e80041fb6513667
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-12 20:07:08 +0530

    [Gyanendra] Refactor: A Program to count the number of string in List is Added

---
+++ data_structure/List/4_string.py
@@ -0,0 +1,34 @@
+"""
+@Author: Gyanendra
+@Date: 10/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 12/01/2021 
+@Title : List in Python
+"""
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def function_smallest_list(list):
+    """
+    Description:
+        Function is Used for count the number of strings where the string length
+        is 2 or more and the first and last character are same from a given list of strings.
+    Parameter:
+        Sample List
+    Return:
+        Count
+    """
+    temp = []
+    for i in list:
+        if (len(i) >+ 2):
+            temp.append(i[0] + i[-1])
+    for item in temp:
+        if temp.count(item) != 1:
+            return temp.count(item)
+        else:
+            return 'Nothing Available as per Requirement'
+
+if __name__ == "__main__":
+    sample_list = ['abc', 'xyz', 'azc', '1221']
+    result = function_smallest_list(sample_list)
+    logging.debug("Smallest item in list {} is {}".format(sample_list, result))
