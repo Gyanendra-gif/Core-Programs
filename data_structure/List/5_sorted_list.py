0674d26602598ba6ebbcd88421ccfa496324ec3f
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-12 20:14:55 +0530

    [Gyanendra] Add: A Program of Sorted the List is Added

---
+++ data_structure/List/5_sorted_list.py
@@ -0,0 +1,33 @@
+"""
+@Author: Gyanendra
+@Date: 12/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 12/01/2021 
+@Title : List in Python
+"""
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def function_smallest_list(list):
+    """
+    Description:
+        Function is Used for sorted list in increasing order by the last
+        element in each tuple from a given list of non-empty tuples.
+    Parameter:
+        Sample List
+    Return:
+        Sorted List
+    """
+    temp = []
+    temp.append(list[0])
+    for i in range(1,5):
+        for item in range(temp.__len__()):
+            if temp[item][1] > list[i][1]:
+                temp.insert(item, list[i])
+                break
+    return temp
+
+if __name__ == "__main__":
+    sample_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+    result = function_smallest_list(sample_list)
+    logging.debug("Items in Increasing Order of List {} is {}".format(sample_list, result))
