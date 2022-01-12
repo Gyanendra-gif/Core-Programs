d78f68a2901e0ced5607e4e985f3374fa722e607
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-12 13:07:54 +0530

    [Gyanendra] Refactor: A Program of Add Data is List is Added

---
+++ data_structure/List/2_multiply_list.py
@@ -0,0 +1,27 @@
+"""
+@Author: Gyanendra
+@Date: 10/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 11/01/2021 
+@Title : List in Python
+"""
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def function_add_list(list):
+    """
+    Description:
+        Function is Used for Multiplies All the items of List 
+    Parameter:
+        Sample List
+    Return:
+        Created Set
+    """
+    count = 0
+    for item in list:
+        count = count *  item
+    return count
+
+sample_list = [1,2,3,4,5,6,7]
+result = function_add_list(sample_list)
+logging.debug("Sum of all items in list {} is {}".format(sample_list, result))
