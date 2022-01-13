d8ae027d96b99e7ff399e747fe72ed1760759874
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-13 09:02:51 +0530

    [Gyanendra] Add: A Program of Sample tuple creation is added

---
+++ data_structure/Tuple/1_create_tuple.py
@@ -0,0 +1,28 @@
+"""
+@Author: Gyanendra
+@Date: 10/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 11/01/2021 
+@Title : Tuple in Python
+"""
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def function_create_tuple(lst):
+    """
+    Description:
+        Function is Used for creating sample tuple from the items of List 
+    Parameter:
+        Sample List
+    Return:
+        Created tuple
+    """
+    sample_tuple = tuple(lst)
+    return sample_tuple
+
+if __name__ == "__main__":
+    sample_list = [1,2,3,4,5,6,7]
+    result = function_create_tuple(sample_list)
+    logging.debug("Here is the created tuple from list {} is {}".format(sample_list, result))
+ 
+  
