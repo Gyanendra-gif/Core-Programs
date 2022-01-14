a116fc7b59c672e8789d05686765b9f3b1aea180
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-14 16:09:18 +0530

    [Gyanendra] Add: A Program for unpacked tuple is added

---
+++ data_structure/Tuple/3_unpack_tuple.py
@@ -0,0 +1,24 @@
+"""
+@Author: Gyanendra
+@Date: 14/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 14/01/2021 
+@Title : Tuple in Python
+"""
+def function_create_tuple():
+    """
+    Description:
+        Function is Used for unpack a tuple in several variables.
+    Parameter:
+        Sample List
+    Return:
+        Created tuple
+    """
+    sample_tuple = (1,2,3) 
+    n1, n2, n3 = sample_tuple
+    print(n1, n2, n3)
+
+if __name__ == "__main__":
+    function_create_tuple()
+ 
+  
