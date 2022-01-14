82238493d88628accf0a7a4fe61cf1e902c6b46f
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-14 16:23:18 +0530

    [Gyanendra] Add: A Program of Colon of Tuple is Added

---
+++ data_structure/Tuple/4_colon_tuple.py
@@ -0,0 +1,29 @@
+"""
+@Author: Gyanendra
+@Date: 14/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 14/01/2021 
+@Title : Tuple in Python
+"""
+import copy
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def function_colon_tuple():
+    """
+    Description:
+        Function is Used for to create the colon of a tuple.
+    Parameter:
+        Sample List
+    Return:
+        Created tuple
+    """
+    sample_tuple = (1,2,[5],6) 
+    colon_tuple = copy.deepcopy(sample_tuple)
+    colon_tuple[2].append(7)
+    logging.debug(sample_tuple)
+    logging.debug(colon_tuple)
+
+if __name__ == "__main__":
+    function_colon_tuple()
+  
