977ce07e3e23acf3448ac51e8bc6d56140d50e20
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-16 14:40:23 +0530

    [Gyanendra] Add: A Program of First Name Validation is Added

---
+++ data_structure/User Registration/1_first_name.py
@@ -0,0 +1,30 @@
+"""
+@Author: Gyanendra
+@Date: 16/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 16/01/2021 
+@Title : User Registration in Python
+"""
+
+import re
+from logging_config import logger
+
+def function_name_validate(name):
+    """
+    Description:
+        Function is Used for Validating the First Name of User
+    Parameter:
+        Sample String as Name
+    Return:
+        None
+    """
+    pattern = r'^[a-zA-Z]{3,30}$'
+    match = re.fullmatch(pattern, name)
+    if match:
+        logger.info('First Name is Valid')
+    else:
+        logger.error('Error! Last Name is Not Valid')
+
+if __name__ == "__main__":
+    user_name = input("Enter Your First Name to Check Validation: ")
+    function_name_validate(user_name)        
---
+++ data_structure/User Registration/logging_config.py
@@ -0,0 +1,18 @@
+from logging import *
+import logging
+import sys
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s', 
+                              '%m-%d-%Y %H:%M:%S')
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('E:/Python Workspace/Core-Programs/data_structure/User Registration/user_registration.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stdout_handler)
