4351ea4a4e593df4ad60e996715ccbb3df9eefb5
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-16 18:57:23 +0530

    [Gyanendra] Add: A Program to Validate Password having atleast one upper case letter, one numeric char. and eight char and one special char

---
+++ data_structure/User Registration/8_password_valid_four.py
@@ -0,0 +1,31 @@
+"""
+@Author: Gyanendra
+@Date: 16/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 16/01/2021 
+@Title : User Registration in Python
+"""
+
+import re
+from logging_config import logger
+
+def function_password_validate(password):
+    """
+    Description:
+        Function is Used for Validating the Password of User with having Eight Characters and one
+        Upper case letter and having one numeric character also with one special char
+    Parameter:
+        Sample String
+    Return:
+        None
+    """
+    pattern = r'(?=.*[A-Z])(?=.*[0-9])(?=.*[@!#$%^&*_-]).{8,}'
+    match = re.fullmatch(pattern, password)
+    if match:
+        logger.info('Entered Password is Valid')
+    else:
+        logger.error('Error! Entered Password is Not Valid')
+
+if __name__ == "__main__":
+    user_input = input('Enter Password atleast having eight char., one uppercase letter and one numeric char.and one specific char to check validation: ')
+    function_password_validate(user_input)        
