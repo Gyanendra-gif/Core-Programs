01c4b4a3a4f42ad667d5df1a4a80f20d1ff10ecc
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2022-01-16 16:19:52 +0530

    [Gyanendra] Add: A Program to Check Password Validation having minimum eight character and one uppercase letter is Added

---
+++ data_structure/User Registration/6_password_valid_two.py
@@ -0,0 +1,30 @@
+"""
+@Author: Gyanendra
+@Date: 16/01/2021 
+@Last Modified by: Gyanendra
+@Last Modified time: 16/01/2021 
+@Title : User Registration in Python
+"""
+
+import re
+from logging_config import logger
+
+def function_password_validate(password):
+    """
+    Description:
+        Function is Used for Validating the Password of User 
+    Parameter:
+        Sample String
+    Return:
+        None
+    """
+    pattern = r'(?=.*[A-Z]).{8,}'
+    match = re.fullmatch(pattern, password)
+    if match:
+        logger.info('Entered Password is Valid')
+    else:
+        logger.error('Error! Entered Password is Not Valid')
+
+if __name__ == "__main__":
+    user_input = input('Enter Password atleast having eight characters and one uppercase letter to check validation: ')
+    function_password_validate(user_input)        
